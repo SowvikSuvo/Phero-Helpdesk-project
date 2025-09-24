1ca55433f80681e2bcd79734046b6f8f693fd915
SowvikSuvo <skarmoker44@gmail.com>
2025-09-24 16:56:32 +0600

    Project setup and added components

---
+++ src/components/Toggles_Btns.jsx
@@ -0,0 +1,45 @@
+import Container from "./Container";
+const Toggles_Btns = ({ toggleStatus, setToggleStatus }) => {
+  return (
+    <div>
+      <Container>
+        <div className="text-right mb-14">
+          <button
+            onClick={() => setToggleStatus("All")}
+            className={`toggle-btn rounded-l-md ${
+              toggleStatus == "All" && "!bg-purple-500 !text-white"
+            }`}
+          >
+            All
+          </button>
+          <button
+            onClick={() => setToggleStatus("Pending")}
+            className={`toggle-btn ${
+              toggleStatus == "Pending" && "!bg-purple-500 !text-white"
+            }`}
+          >
+            Pending
+          </button>
+          <button
+            onClick={() => setToggleStatus("Submitted")}
+            className={`toggle-btn ${
+              toggleStatus == "Submitted" && "!bg-purple-500 !text-white"
+            }`}
+          >
+            Submitted
+          </button>
+          <button
+            onClick={() => setToggleStatus("Reviewed")}
+            className={`toggle-btn rounded-r-md ${
+              toggleStatus == "Reviewed" && "!bg-purple-500 !text-white"
+            }`}
+          >
+            Reviewed
+          </button>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Toggles_Btns;
